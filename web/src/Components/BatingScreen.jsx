ed9681e583789da8aea994cd976571239ce342a0
jaffar aman <amanjaffar50@gmail.com>
2021-11-20 21:51:46 +0500

    socket uri

--- web/src/Components/BatingScreen.jsx
+++ web/src/Components/BatingScreen.jsx
@@ -24,7 +24,7 @@ const BatingScreen = () => {
   }, []);
 
   useEffect(() => {
-    const socket = io("http://localhost:5000");
+    const socket = io();
 
     socket.on("connect", function () {
       console.log("connected to server");
